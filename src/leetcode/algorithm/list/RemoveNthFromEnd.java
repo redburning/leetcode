366f1ba85d2d0f883bd884c1b26c764e6b6a57cb
neusoft <neusoft@DESKTOP-S4LIPHE>
2020-01-06 20:27:18 +0800

    19. 删除链表的倒数第N个节点

---
+++ src/leetcode/algorithm/list/RemoveNthFromEnd.java
@@ -0,0 +1,32 @@
+package leetcode.algorithm.list;
+
+public class RemoveNthFromEnd {
+
+	class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int val) {
+			this.val = val;
+		}
+	}
+	
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode first = head;
+        ListNode second = head;
+        int cnt = 0;
+        while (cnt < n && first.next != null) {
+        	first = first.next;
+        	cnt++;
+        }
+        if (first.next == null && cnt == n - 1)
+        	return head.next;
+        
+        while (first.next != null) {
+        	first = first.next;
+        	second = second.next;
+        }
+        second.next = second.next.next;
+        return head;
+    }
+	
+}
